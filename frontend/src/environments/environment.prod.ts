54b6298f57969855dc7f484e0c8c9b74e98161a8
Doniel Eunson Ko <deko74415@liceo.edu.ph>
2026-04-16 18:14:12 +0800

    Add Google Client ID to production environment

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: '/api/v1'
+  apiUrl: '/api/v1',
+  googleClientId: '833154652410-8i28an33pvqpa1t20movdjka1tke6p0q.apps.googleusercontent.com'
 };
