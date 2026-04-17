28550d07a8d54787790ef39d8ce6da722438d095
Doniel Eunson Ko <deko74415@liceo.edu.ph>
2026-04-18 00:52:11 +0800

    Cleanup: Removed old scratch files and one-off database verification scripts from backend and root directories

--- backend/scratch/gen-hash.js
+++
@@ -1,7 +0,0 @@
-const bcrypt = require('bcryptjs');
-
-const password = 'Admin@2024';
-const salt = bcrypt.genSaltSync(12);
-const hash = bcrypt.hashSync(password, salt);
-
-console.log('HASH:', hash);
